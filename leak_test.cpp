1f2d50b88628dc32a8ca86fe7c03afc37ee671aa
Иван Лаврущев <ivan_olympiad@mail.ru>
2024-12-16 05:21:23 +0300

    memory leak fix

--- leak_test.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main() {
-    int* pointer = NULL;
-    for(size_t i = 0; i < 10; i++)
-        pointer = (int*)calloc(5, sizeof(int));
-    pointer = NULL;
-}
